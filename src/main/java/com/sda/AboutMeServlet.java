18f4f6e754fda3581462bcf97732ad42c6296013
KamilO <wheels4@o2.pl>
2020-07-19 12:53:19 +0200

    add web pages

---
+++ src/main/java/com/sda/AboutMeServlet.java
@@ -0,0 +1,30 @@
+package com.sda;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AboutMeServlet extends HttpServlet {
+
+    private final static Logger logger = Logger.getLogger(AboutMeServlet.class);
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("I should be initialized after ProgDashboard");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+        writer.println("<p>Kamil Olszewski</p>");
+    }
+}
---
+++ src/main/java/com/sda/ProgrammingLanguageDashboardServlet.java
@@ -0,0 +1,55 @@
+package com.sda;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ProgrammingLanguageDashboardServlet extends HttpServlet {
+
+    private final static Logger logger = Logger.getLogger(AboutMeServlet.class);
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("I should be initialized first");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<style>");
+        writer.println("table {margin:auto; width:60%; border: 2px solid black},");
+        writer.println("th, td {border: 2px solid black;},");
+        writer.println("</style>");
+
+        writer.println("</head>");
+
+        writer.println("<body>");
+///, style="float:center", style = "border: 2px solid black"
+        writer.println(" <table >");
+        writer.println("<tr>");
+        writer.println("<th>Programming language</th> <th>Application</th> <th>difficulty</th>");
+        writer.println("</tr>");
+        writer.println("<tr>");
+        writer.println("<td>Java</td> <td>WEB, Desktop, Mobile</td> <td>3/5</td>");
+        writer.println("</tr>");
+        writer.println("<tr>");
+        writer.println("<td>PHP</td> <td>WEB</td> <td>2/5</td>");
+        writer.println("</tr>");
+
+        writer.println("</body>");
+        writer.println("</html>");
+
+
+    }
+}
